0dc432ea818c0834839c9100ea8df589cb2004d5
Ture Claussen <ture.cl.business@gmail.com>
2022-08-03 22:54:30 +0200

    Fixed lacking color change on project switch

--- src/gcp.js
+++ src/gcp.js
@@ -1,31 +1,51 @@
-let banner = document.getElementsByClassName("cfc-platform-bar-container");
 const colorMap = {
+  "default": "blue",
   "bachelor-thesis-332216": "green",
   "gcp-it-dr-dropstock2-dev-j7f": "green",
   "gcp-it-dr-dropstock2-stage-ka5": "orange",
   "gcp-it-dr-dropstock2-live-3nw": "red",
 };
-const urlParams = new URLSearchParams(window.location.search);
+
+function listenForUrlChange() {
+  let previousUrl = '';
+  const observer = new MutationObserver(function(mutations) {
+    if (window.location.href !== previousUrl) {
+        previousUrl = window.location.href;
+        changeBarColor();
+      }
+  });
+  const config = {subtree: true, childList: true};
+
+  // start listening to changes
+  observer.observe(document, config);
+}
+
 
 function changeBarColor() {
+  const banner = document.getElementsByClassName("cfc-platform-bar-container");
+  const urlParams = new URLSearchParams(window.location.search);
+
   if (banner.length != 1) {
+  console.log('Navigation bar identifier not unique!')
+  console.log(banner);
+  console.log(banner.length);
     throw "Navigation bar identifier not unique!";
   }
 
   if (!urlParams.has("project")) {
+  console.log('Url does not contain project query param!')
     throw "Url does not contain project query param!";
   }
 
   const projectId = urlParams.get("project");
 
-  if (!colorMap.hasOwnProperty(projectId)) {
-    console.log(`No color defined for project with ID: ${projectId}`)
-    exit(0);
-  }
+  const barColor = colorMap.hasOwnProperty(projectId)
+    ? colorMap[projectId]
+    : colorMap["default"];
 
-  banner = banner[0]
-  console.log("Changing color of nav bar")
-  banner.style.backgroundColor = colorMap[projectId];
+  console.log("Changing color of nav bar to " + barColor);
+  banner[0].style.backgroundColor = barColor;
 }
 
 changeBarColor();
+listenForUrlChange();
